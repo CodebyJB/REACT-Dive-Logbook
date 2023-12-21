c3a54660ed0248b0ebf7f09f6283a482ebd986a0
Jennifer <106908910+JenniferBraunbarth@users.noreply.github.com>
2023-12-21 09:44:35 +0100

    update

--- data/middleware.js
+++
@@ -1,6 +0,0 @@
-// middleware.js
-module.exports = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-};
